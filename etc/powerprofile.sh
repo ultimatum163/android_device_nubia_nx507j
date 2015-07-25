cc9af3d09c46a66678b0ed359e572bba084842c9
pexcn <pexcn@qq.com>
2015-07-25 13:56:12 +0800

    fix powerprofile.sh

--- etc/powerprofile.sh
+++ etc/powerprofile.sh
@@ -6,15 +6,23 @@ dev_governor=`ls /sys/class/devfreq/qcom,cpubw*/governor`
             "2")
                  stop mpdecision
                  sleep 0.5
+                 echo 1                                    > /sys/devices/system/cpu/cpu0/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu1/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu2/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu3/online
                  echo 1958400                              > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                  echo 1958400                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 1958400                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 1958400                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+                 echo 300000                               > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+                 echo 300000                               > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+                 echo 300000                               > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+                 echo 300000                               > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
                  echo performance                          > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
                  echo performance                          > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
                  echo performance                          > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
                  echo performance                          > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-                 echo 1                                    > /sys/devices/system/cpu/sched_mc_power_savings
+                 echo 0                                    > /sys/devices/system/cpu/sched_mc_power_savings
                  echo 578000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
                  echo msm-adreno-tz                        > /sys/class/kgsl/kgsl-3d0/devfreq/governor
                  echo "msm_cpufreq"                        > $dev_governor
